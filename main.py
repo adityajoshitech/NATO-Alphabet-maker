f6aa664ddbd5df1ffe61589c0df74335472d1fd8
Aditya Joshi <5adityajoshi@gmail.com>
2026-06-02 20:13:43 +0530

    Create generate_phonetic function for user input
    Refactor phonetic word generation into a function with error handling.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas
-
 df=pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_words={row.letter:row.code for index,row in df.iterrows()}
 
 
-word=input("Enter the word: ").upper()
-phonetic_word=[phonetic_words[i] for i in word]
-print(phonetic_word)
+def generate_phonetic():
+    word=input("Enter the word: ").upper()
+    try:
+        phonetic_word=[phonetic_words[i] for i in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_word)
+generate_phonetic()
